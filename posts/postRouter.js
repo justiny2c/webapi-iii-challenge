b80973dde56606ca0a423a54edc071a1c3b78d38
justiny2c <justiny2c@gmail.com>
2019-07-10 15:49:54 -0700

    MVP complete

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -16,21 +16,53 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+    const id = req.params.id
 
+    dbPost
+    .getById(id)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
 });
 
-router.delete('/:id', (req, res) => {
+// router.delete('/:id', (req, res) => {
+//     dbPost
+//     .remove()
+// });
 
-});
+router.put('/:id', validatePostId, (req, res) => {
+    const post = req.body
+    const id = req.params.id
 
-router.put('/:id', (req, res) => {
-
+    dbPost
+    .update(id, post)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
+    const id = req.params.id
 
+    dbPost
+    .getById(id)
+    .then(post => {
+        if(!post){
+            res.status(404).json( {error: "ID not found" } )
+        } else {
+            next();
+        }})
+    .catch(error => {
+        res.status(500).json( {error: "Something went wrong"} )
+    })
 };
 
 module.exports = router;
